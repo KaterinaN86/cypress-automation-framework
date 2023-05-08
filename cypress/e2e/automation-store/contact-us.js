94fded87766e01a7687c6ad7de8542d62508f442
Katerina Naumova <112091580+KaterinaN86@users.noreply.github.com>
2023-05-08 13:30:43 +0200

    Added configuration for working with XPath

--- cypress/e2e/automation-store/contact-us.js
+++ cypress/e2e/automation-store/contact-us.js
@@ -1,7 +1,12 @@
 /// <reference types="Cypress" />
-describe('Test contact us form via Atuomation Test Store', () => {
-    //happy path test, filling in concat form on webuniversitu site.
+describe('Test contact us form via Automation Test Store', () => {
+    //happy path test, filling in concat form on automationstore site.
     it('Should be able to submit successfully filled out form', () => {
+        //Open automation store site.
         cy.visit('https://automationteststore.com/');
+        //Click 'contact us' link
+        cy.get('a:contains(Contact Us)').should('be.visible').click();
+        //Fill out form
+        cy.get("[name='first_name']").type('Joe');
     });
 });
