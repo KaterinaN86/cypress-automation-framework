6cec30b2ad4997dd3338d3e83f00dbbf34482089
Katerina Naumova <112091580+KaterinaN86@users.noreply.github.com>
2023-05-10 18:44:30 +0200

    Updated automation-store contact us test suite
    Added Xpath selectors examples.

--- README.md
+++ README.md
@@ -5,4 +5,8 @@
 ## Tests (specs) can be found in directory **cypress/e2e**
 
 -   **automation-store** tests
-    -   **contact-us.js** contains Cypress tests with happy and unhappy path regarding contact-us form on site [https://automationteststore.com/](https://automationteststore.com/).
+
+    -   **contact-us.js** contains Cypress tests with happy and unhappy path regarding contact-us form on site [https://automationteststore.com/](https://automationteststore.com/). XPath locators are used in both tests of test suite to demonstrate XPath selectors. Documentation on Xpath expressions can be found on [https://www.w3schools.com/xml/xpath_intro.asp](https://www.w3schools.com/xml/xpath_intro.asp) and [https://www.tutorialspoint.com/xpath/xpath_expression.htm](https://www.tutorialspoint.com/xpath/xpath_expression.htm).
+
+-   **webdriver-uni** tests
+    -   **contact-us.js** script contains test suite that **contact us** form on [www.webdriveruniversity.com](www.webdriveruniversity.com). It has two test cases, one describing a happy path where all data is entered in correct format and one following an unhappy path with missing data preventing form submission.
--- cypress/e2e/automation-store/contact-us.js
+++ cypress/e2e/automation-store/contact-us.js
@@ -4,9 +4,28 @@ describe('Test contact us form via Automation Test Store', () => {
     it('Should be able to submit successfully filled out form', () => {
         //Open automation store site.
         cy.visit('https://automationteststore.com/');
-        //Click 'contact us' link
-        cy.get('a:contains(Contact Us)').should('be.visible').click();
+        //Click 'contact us' link, using XPath selector.
+        cy.xpath("//a[contains(@href,'contact')]").should('be.visible').click();
         //Fill out form
         cy.get("[name='first_name']").type('Joe');
+        cy.get("[id='ContactUsFrm_email']").type('someEmail@gmail.com');
+        //Using XPath to select text area.
+        cy.xpath('//textarea[@rows=8]').type('How do I return a product?');
+        //Using XPath axis to select submit button element in contact us form.
+        cy.xpath('//div[@class="col-md-6 col-sm-6"]/child::button').click();
+    });
+    //Unhappy path, contact us form is filled in but submit fails because of missing e-mail.
+    it('Should not be able to submit contact us form with missing e-mail data.', () => {
+        //Open automation store site.
+        cy.visit('https://automationteststore.com/');
+        //Click 'contact us' link, using XPath selector.
+        cy.xpath("//a[contains(@href,'contact')]").should('be.visible').click();
+        //Fill out form.
+        cy.get("[name='first_name']").type('Joe');
+        //Using XPath to select text area.
+        cy.xpath('//textarea[@rows=8]').type('How do I return a product?');
+        //Using XPath axis to select submit button element in contact us form.
+        //A message about invalid E-mail Address is displayed when submit button is clicked.
+        cy.xpath('//div[@class="col-md-6 col-sm-6"]/child::button').click();
     });
 });
